5100370ac7b69696a42917e84ee7ad3719aa00f8
Matthew Hewitson <matthew.hewitson@btinternet.com>
2022-09-20 22:45:12 +0100

    Fix up sym layer so it matches UK layout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+#define MIRYOKU_ALTERNATIVES_SYM \
+&kp LBRC,               &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &to U_BASE,        &to U_EXTRA,       &to U_TAP,         &bootloader,       \
+&kp COLON,              &kp DLLR,          &kp PRCT,          &kp CRRT,          &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp LS(NON_US_HASH),    &kp EXCL,          &kp LS(APOS),      &kp NON_US_HASH,   &kp PIPE2,         U_NA,              &to U_SYM,         &to U_MOUSE,       &kp RALT,          U_NA,              \
+U_NP,                   U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
