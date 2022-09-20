b54adcea9ba2e38315540ba0901507c0a867591b
Matthew Hewitson <matthew.hewitson@btinternet.com>
2022-09-20 22:52:52 +0100

    Fix name of define

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,6 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-#define MIRYOKU_ALTERNATIVES_SYM \
+#define MIRYOKU_LAYER_SYM \
 &kp LBRC,               &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &to U_BASE,        &to U_EXTRA,       &to U_TAP,         &bootloader,       \
 &kp COLON,              &kp DLLR,          &kp PRCT,          &kp CRRT,          &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp LS(NON_US_HASH),    &kp EXCL,          &kp LS(APOS),      &kp NON_US_HASH,   &kp PIPE2,         U_NA,              &to U_SYM,         &to U_MOUSE,       &kp RALT,          U_NA,              \
